c687e4aecee31ada5bc06ecff56798a8ca3858e2
Rodion Efremov <coderodd3@yandex.com>
2024-06-06 13:39:21 +0300

    Moving on

--- src/main/java/com/github/coderodde/game/connect4fx/ConnectFourFXCanvas.java
+++ src/main/java/com/github/coderodde/game/connect4fx/ConnectFourFXCanvas.java
@@ -1,6 +1,9 @@
 package com.github.coderodde.game.connect4fx;
 
 import com.github.coderodde.game.connect4.ConnectFourBoard;
+import static com.github.coderodde.game.connect4.ConnectFourBoard.COLUMNS;
+import static com.github.coderodde.game.connect4.ConnectFourBoard.ROWS;
+import com.github.coderodde.game.zerosum.PlayerType;
 import java.awt.Point;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.Canvas;
@@ -9,20 +12,35 @@ import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 
 /**
- *
- * @author PotilasKone
+ * This class implements the canvas for drawing the game board.
+ * 
+ * @version 1.0.0 (Jun 6, 2024)
+ * @since 1.0.0 (Jun 6, 2024)
  */
 public final class ConnectFourFXCanvas extends Canvas {
     
     private static final Color BACKGROUND_COLOR = Color.valueOf("#295c9e");
+    private static final Color AIM_COLOR        = Color.valueOf("#167a0f");
+    
+    private static final Color HUMAN_PLAYER_CELL_COLOR = 
+            Color.valueOf("#bfa730");
+    
+    private static final Color AI_PLAYER_CELL_COLOR = 
+            Color.valueOf("#b33729");
+    
     private static final double CELL_LENGTH_SUBSTRACT = 10.0;
     private static final double RADIUS_SUBSTRACTION_DELTA = 10.0;
+    private static final int CELL_Y_NOT_FOUND = -1;
+    private static final int INITIAL_AIM_X = 3;
     
-    private final Point previousCellPoint = new Point(-1, -1);    
-    private ConnectFourBoard board;
+    private int previousAimX = INITIAL_AIM_X;
+    private ConnectFourBoard board = new ConnectFourBoard();
     private double cellLength;
     
     public ConnectFourFXCanvas() {
+        board = board.makePly(2, PlayerType.MINIMIZING_PLAYER);
+        board = board.makePly(4, PlayerType.MAXIMIZING_PLAYER);
+        
         setSize();
         paintBackground();
         
@@ -33,12 +51,6 @@ public final class ConnectFourFXCanvas extends Canvas {
         this.addEventFilter(MouseEvent.MOUSE_CLICKED, e -> {
         
         });
-        
-//        final EventHandler<MouseEvent> mouseHandler = 
-//                new ConnectFourFXCanvasMouseHandler();
-        
-//        this.setOnMouseClicked(mouseHandler);
-//        this.setOnMouseMoved(mouseHandler);
     }
     
     public void hit(final int x) {
@@ -46,13 +58,88 @@ public final class ConnectFourFXCanvas extends Canvas {
     }
     
     private void processMouseMoved(final MouseEvent mouseEvent) {
-        final Point point = convertMouseLocationToCellPoint(mouseEvent);
+        final double mouseX = mouseEvent.getSceneX();
+        final int cellX = (int)(mouseX / cellLength);
+        final int emptyCellY = getEmptyCellYForX(cellX);
         
-        if (point == null) {
-            
+        if (cellX == previousAimX) {
+            // Nothing changed.
+            return;
         }
         
+        previousAimX = cellX;
         
+        if (emptyCellY == CELL_Y_NOT_FOUND) {
+            return;
+        }
+        
+        paintBackground();
+        paintBoard();
+        paintCellSelection(cellX, emptyCellY);
+    }
+    
+    private void paintBoard() {
+        for (int y = 0; y < ROWS; y++) {
+            for (int x = 0; x < COLUMNS; x++) {
+                final PlayerType playerType = board.get(x, y);
+                
+                paintCell(playerType, x, y);
+            }
+        }
+    }
+    
+    private void paintCell(final PlayerType playerType, 
+                           final int x,
+                           final int y) {
+        
+        if (playerType == null) {
+            return;
+        }
+        
+        final double topLeftX = cellLength * x;
+        final double topLeftY = cellLength * y;
+        final Color color = 
+                playerType == PlayerType.MINIMIZING_PLAYER ?
+                            HUMAN_PLAYER_CELL_COLOR :
+                            AI_PLAYER_CELL_COLOR;
+                            
+        this.getGraphicsContext2D().setFill(color);
+        this.getGraphicsContext2D()
+            .fillOval(topLeftX, 
+                      topLeftY, 
+                      cellLength, 
+                      cellLength);
+    }
+    
+    private void paintCellSelection(final int cellX, final int cellY) {
+        final double topLeftX = cellLength * cellX;
+        final double topLeftY = cellLength * cellY;
+        final double innerWidth = cellLength - 2.0 * RADIUS_SUBSTRACTION_DELTA;
+        
+        this.getGraphicsContext2D().setFill(AIM_COLOR);
+        this.getGraphicsContext2D()
+                .fillOval(
+                        topLeftX, 
+                        topLeftY,
+                        cellLength,
+                        cellLength);
+        
+        this.getGraphicsContext2D().setFill(Color.WHITE);
+        this.getGraphicsContext2D()
+                .fillOval(topLeftX + RADIUS_SUBSTRACTION_DELTA, 
+                          topLeftY + RADIUS_SUBSTRACTION_DELTA, 
+                          innerWidth, 
+                          innerWidth);
+    }
+    
+    private int getEmptyCellYForX(final int cellX) {
+        for (int y = ROWS - 1; y >= 0; y--) {
+            if (board.get(cellX, y) == null) {
+                return y;
+            }
+        }
+        
+        return CELL_Y_NOT_FOUND;
     }
     
     private Point convertMouseLocationToCellPoint(final MouseEvent mouseEvent) {
@@ -98,7 +185,7 @@ public final class ConnectFourFXCanvas extends Canvas {
         final Rectangle2D primaryScreenBounds =
                 Screen.getPrimary().getVisualBounds();
         
-        final double verticalLength = 
+        final double verticalLength =  
                 primaryScreenBounds.getHeight() / ConnectFourBoard.ROWS;
         
         final double horizontalLength = 
